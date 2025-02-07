ed5349ecc55da8e7b5779b5f027fcb0f9e262c3f
Minoo-Yh <minoo.yaghoubi@gmail.com>
2025-02-07 13:59:13 +0200

    My react native expo instagram app

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+// for share screen between all screen 
+// stack like cholderen
+// Tabs footer tab 
+
+import {Stack} from 'expo-router';
+
+export default function RootLayout(){
+    return  <Stack/>;
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+//first screen with url 
+import { Redirect } from 'expo-router';
+import {Text} from 'react-native';
+
+export default function Home() {
+    return < Redirect href="/(tabs)"/>;
+    
+}
